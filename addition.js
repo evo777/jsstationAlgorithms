e7814e524a71887dc12839f17a9360227b369b41
Emmanuel Vo <emmanuelvo777@gmail.com>
2018-03-23 10:50:02 -0700

    Added double quotes

--- addition.js
+++ addition.js
@@ -1,6 +1,6 @@
 {
-  title: "Addition",
-  description: "Write a function that adds two numbers together",
-  labels: ["Easy", "Number"],
-  codeInput: "const addition = (a, b) => {}"
+  "title": "Addition",
+  "description": "Write a function that adds two numbers together",
+  "labels": ["Easy", "Number"],
+  "codeInput": "const addition = (a, b) => {}"
 }
