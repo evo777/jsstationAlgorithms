05b649828ffff164c1d26ff0f1a2dec537190c19
Emmanuel Vo <emmanuelvo777@gmail.com>
2018-03-30 11:48:41 -0700

    Updated

--- addition.js
+++ addition.js
@@ -2,5 +2,11 @@
   "title": "Addition",
   "description": "Write a function that adds two numbers together",
   "labels": ["Easy", "Number"],
-  "codeInput": "const addition = (a, b) => {}"
+  "codeInput": "const addition = (a, b) => {}",
+  "approved": "true",
+  "createdAt": "1233523461",
+  "markdown": "mocha test",
+  "updateAt": "1234534209753",
+  "filterSrc": "filter",
+  "id": "k28ahs"
 }
