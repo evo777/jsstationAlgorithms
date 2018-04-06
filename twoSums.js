2d312adf98f54536fbbead65355196dfb9cf6ecd
Emmanuel Vo <emmanuelvo777@gmail.com>
2018-04-06 14:19:49 -0700

    Updated data

--- twoSums.js
+++ twoSums.js
@@ -2,5 +2,11 @@
  " title": "Two Sums",
   "description": "Find two indices that two numbers add up to the target.",
   "labels": ["Easy", "Array"],
-  "codeInput": "const twoSums = (array, sum) => {}"
+  "codeInput": "const twoSums = (array, sum) => {}",
+  "approved": "true",
+  "createdAt": "123345226461",
+  "markdown": "mocha test",
+  "updateAt": "1239004209373",
+  "filterSrc": "filter",
+  "id": "sd22dsgaxcbs"
 }
